01ae4d72095c68f8bdf44c2a8306595dc7c66ca7
Nicolas Moon <nmoon@lightspeedsystems.com>
2025-01-19 22:55:43 -0600

    i changed things

--- nvim/lua/moon/remap/init.lua
+++ nvim/lua/moon/remap/init.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", ":Telescope file_browser<CR>")
 vim.keymap.set("n", "<leader>h", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end)
+vim.api.nvim_set_keymap('n', '<leader>y', '"+y', { noremap = true, silent = true })
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
