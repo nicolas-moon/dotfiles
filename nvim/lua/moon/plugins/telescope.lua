600d2b074a45541d06e71d452863c63f0055c43a
Nicolas Moon <nmoon@lightspeedsystems.com>
2025-01-21 14:25:50 -0600

    adjust telecope to ignore '.git' folder but still include '.github'

--- nvim/lua/moon/plugins/telescope.lua
+++ nvim/lua/moon/plugins/telescope.lua
@@ -1,78 +1,79 @@
 return {
-    "nvim-telescope/telescope.nvim",
+  "nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
+  tag = "0.1.5",
 
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        {
-            'nvim-telescope/telescope-fzf-native.nvim',
-            build =
-            'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES=arm64 && cmake --build build --config Release && cmake --install build --prefix build'
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build =
+      'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES=arm64 && cmake --build build --config Release && cmake --install build --prefix build'
+    }
+  },
+
+  config = function()
+    local telescope = require('telescope')
+
+    telescope.setup {
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-j>"] = require('telescope.actions').move_selection_next,
+            ["<C-k>"] = require('telescope.actions').move_selection_previous,
+          },
+          n = {
+            ["<C-j>"] = require('telescope.actions').move_selection_next,
+            ["<C-k>"] = require('telescope.actions').move_selection_previous,
+          },
+        },
+        file_ignore_patterns = {
+          "node_modules",
+          "target",
+          "%.git/",
+        },
+        vimgrep_arguments = {
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case',
+          '--hidden',
+        },
+      },
+      pickers = {
+        find_files = {
+          hidden = true
         }
-    },
-
-    config = function()
-        local telescope = require('telescope')
-
-        telescope.setup {
-            defaults = {
-                mappings = {
-                    i = {
-                        ["<C-j>"] = require('telescope.actions').move_selection_next,
-                        ["<C-k>"] = require('telescope.actions').move_selection_previous,
-                    },
-                    n = {
-                        ["<C-j>"] = require('telescope.actions').move_selection_next,
-                        ["<C-k>"] = require('telescope.actions').move_selection_previous,
-                    },
-                },
-                file_ignore_patterns = {
-                    "node_modules",
-                    "target",
-                },
-                vimgrep_arguments = {
-                    'rg',
-                    '--color=never',
-                    '--no-heading',
-                    '--with-filename',
-                    '--line-number',
-                    '--column',
-                    '--smart-case',
-                    '--hidden',
-                },
-            },
-            pickers = {
-                find_files = {
-                    hidden = true
-                }
-            },
-            extensions = {
-                fzf = {
-                    fuzzy = true,
-                    override_generic_sorter = true,
-                    override_file_sorter = true,
-                    case_mode = "smart_case"
-                }
-            }
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case"
         }
+      }
+    }
 
-        telescope.load_extension('fzf')
+    telescope.load_extension('fzf')
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pws', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>pWs', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-    end
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>pws', function()
+      local word = vim.fn.expand("<cword>")
+      builtin.grep_string({ search = word })
+    end)
+    vim.keymap.set('n', '<leader>pWs', function()
+      local word = vim.fn.expand("<cWORD>")
+      builtin.grep_string({ search = word })
+    end)
+    vim.keymap.set('n', '<leader>ps', function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end)
+    vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+  end
 }
