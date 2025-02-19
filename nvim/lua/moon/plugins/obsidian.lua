238521d9dddbc3a3f6de2a65c21719c2cf797b51
Nicolas Moon <nmoon@lightspeedsystems.com>
2025-02-19 17:43:57 -0600

    add obsidian

---
+++ nvim/lua/moon/plugins/obsidian.lua
@@ -0,0 +1,42 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+
+    -- see below for full list of optional dependencies 👇
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "/Users/nickmoon/Library/Mobile Documents/iCloud~md~obsidian/Documents/personal",
+      },
+      {
+        name = "work",
+        path = "/Users/nickmoon/Library/Mobile Documents/iCloud~md~obsidian/Documents/work",
+      },
+    },
+
+    -- see below for full list of options 👇
+    -- Optional, customize how note file names are generated given the ID, target directory, and title.
+    ---@param spec { id: string, dir: obsidian.Path, title: string|? }
+    ---@return string|obsidian.Path The full path to the new note.
+    note_path_func = function(spec)
+      -- This is equivalent to the default behavior.
+      local path = spec.dir / tostring(spec.title)
+      return path:with_suffix(".md")
+    end,
+  },
+}
