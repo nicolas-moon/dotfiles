d11f5e5f6a1619f38d66d408f6718ab16d01f178
Nicolas Moon <nick@nickmoon.rocks>
2025-02-23 08:30:45 -0600

    add styling and new keys

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,61 +5,84 @@ local config = wezterm.config_builder()
 config.color_scheme = 'catppuccin-mocha'
 config.macos_window_background_blur = 30
 config.window_background_opacity = 0.91
-config.font = wezterm.font('JetBrains Mono')
+config.font = wezterm.font {
+  family = 'JetBrains Mono',
+  weight = 'Medium',
+}
+config.font_size = 14.0
+config.line_height = 1.0
+
+-- slow blinking cursor
+config.default_cursor_style = 'BlinkingUnderline'
 
 config.keys = {
-    {
-        key = 'd',
-        mods = 'CMD',
-        action = act.SplitVertical,
-    },
-    {
-        key = 'd',
-        mods = 'CMD|SHIFT',
-        action = act.SplitHorizontal,
-    },
-    {
-        key = 'h',
-        mods = 'CMD|SHIFT',
-        action = act.ActivatePaneDirection 'Left',
-    },
-    {
-        key = 'l',
-        mods = 'CMD|SHIFT',
-        action = act.ActivatePaneDirection 'Right',
-    },
-    {
-        key = 'k',
-        mods = 'CMD|SHIFT',
-        action = act.ActivatePaneDirection 'Up',
-    },
-    {
-        key = 'j',
-        mods = 'CMD|SHIFT',
-        action = act.ActivatePaneDirection 'Down',
-    },
-    {
-        key = "e",
-        mods = "CMD|SHIFT",
-        action = act.PromptInputLine {
-            description = "Enter new name for tab",
-            action = wezterm.action_callback(function(window, pane, line)
-                if line then
-                    window:active_tab():set_title(line)
-                end
-            end),
-        }
-    },
-    {
-        key = "f",
-        mods = "CMD|SHIFT",
-        action = wezterm.action_callback(function(window, pane, line)
-            local cwd_uri = pane:get_current_working_dir()
-            if cwd_uri then
-                local cwd = tostring(cwd_uri):match(".*/([^/]+)$") or toastring(cwd_uri)
-                window:active_tab():set_title(cwd)
-            end
-        end),
-    },
+  {
+    key = 'd',
+    mods = 'CMD',
+    action = act.SplitVertical,
+  },
+  {
+    key = 'd',
+    mods = 'CMD|SHIFT',
+    action = act.SplitHorizontal,
+  },
+  {
+    key = 'h',
+    mods = 'CMD|SHIFT',
+    action = act.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'l',
+    mods = 'CMD|SHIFT',
+    action = act.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'k',
+    mods = 'CMD|SHIFT',
+    action = act.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'j',
+    mods = 'CMD|SHIFT',
+    action = act.ActivatePaneDirection 'Down',
+  },
+  {
+    key = 'k',
+    mods = 'CMD',
+    action = act.ClearScrollback 'ScrollbackOnly',
+  },
+  {
+    key = 'p',
+    mods = 'CMD|SHIFT',
+    action = act.ActivateCommandPalette,
+  },
+  {
+    key = 'w',
+    mods = 'CTRL',
+    action = act.CloseCurrentPane { confirm = false },
+  },
+  {
+    key = "e",
+    mods = "CMD|SHIFT",
+    action = act.PromptInputLine {
+      description = "Enter new name for tab",
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    }
+  },
+  {
+    key = "f",
+    mods = "CMD|SHIFT",
+    action = wezterm.action_callback(function(window, pane, line)
+      local cwd_uri = pane:get_current_working_dir()
+      if cwd_uri then
+        local cwd = tostring(cwd_uri):match(".*/([^/]+)$") or toastring(cwd_uri)
+        window:active_tab():set_title(cwd)
+      end
+    end),
+  },
 }
 return config
