ee2234aa1c1c013974935bf0133e40535a98392f
Nicolas Moon <nick@nickmoon.rocks>
2026-02-17 21:48:17 -0600

    updates

--- nvim/lua/moon/remap/init.lua
+++ nvim/lua/moon/remap/init.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pp", ":Explore <CR>")
 vim.keymap.set("n", "<leader>pv", ":Telescope file_browser<CR>")
 vim.keymap.set("n", "<leader>h", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end)
 vim.api.nvim_set_keymap('n', '<leader>y', '"+y', { noremap = true, silent = true })
